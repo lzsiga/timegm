3fe4603fd3d35afcfd52686752e4799b1b2afa57
Lorinczy Zsigmond <zsigmond.lorinczy@idomsoft.hu>
2021-08-16 07:31:52 +0200

    Another test-program

---
+++ meq-test.c
@@ -0,0 +1,50 @@
+/* meq-test.c */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "julian-timegm.h"
+
+/* source:
+   https://astronomy.stackexchange.com/questions/43283/accuracy-of-calculating-the-vernal-equinox/43284#43339
+ */
+
+static void DoOne (int y);
+
+int main (void) {
+    int i;
+
+    for (i=-999; i<2100; ++i) {
+        DoOne (i);
+    }
+    return 0;
+}
+
+static void DoOne (int y) {
+static const double jd0 = 2451545.0;
+static const char tfmt[] = "%Y-%m-%d.%H:%M";
+    int dy;
+    double jd;
+    int64_t ts;
+    struct tm jtm, gtm;
+    char sj [32];
+    char sg [32];
+
+    dy = y - 2000;
+    jd = jd0 + 78.814 +
+        ((((-4.5e-16)* dy - 2.87e-12)*dy + 5.004e-8)*dy + 365.24236)*dy;
+    julian_d2tstamp (jd, &ts);
+
+    {
+       time_t t= ts;
+
+       julian_gmtime_r (&t, &jtm);
+       gmtime_r (&t, &gtm);
+    }
+
+    strftime (sg, sizeof sg, tfmt, &gtm);
+    strftime (sj, sizeof sj, tfmt, &jtm);
+    printf ("%5d: PJC: %16s  PGC: %16s\n", y, sj, sg);
+}
